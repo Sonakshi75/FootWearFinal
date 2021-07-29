f876d02b5245f2c4f5d983448c675f0173545f73
Sonakshi <sonakshimugrai7@gmail.com>
2021-07-29 20:07:51 +0530

    another commit

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ app.set('view engine', 'ejs');
 app.use("/public",express.static(__dirname + "/public"));
 
 app.get("/",function(req,res){
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/home",function(req,res){
   res.render("home");
 });
 
